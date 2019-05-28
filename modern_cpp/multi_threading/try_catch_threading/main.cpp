75f1eb6e3670f767f83726e823417378089a46f2
akshays0911 <swaminathanakshay@gmail.com>
2019-05-28 02:55:49 -0600

    Update with thread id and move semantics

--- modern_cpp/multi_threading/try_catch_threading/main.cpp
+++ modern_cpp/multi_threading/try_catch_threading/main.cpp
@@ -16,6 +16,11 @@ int main(int argc, char *argv[]){
 	//If you define the method then it can be
 	//std::thread child_thread(p)
 	//where p is the object
+    
+    /*
+	Each threaad as unique identification
+    */
+	std::cout << std::this_thread::get_id() << std::endl;
 
 	/*
 	Defining it with the class itself
@@ -30,8 +35,31 @@ int main(int argc, char *argv[]){
 	*/
 	std::thread child_thread((PrintSomething()), std::ref(message));
 
-	child_thread.join();
+	/*
+	It is ineffecient to copy the values
+	*/
+	//or std::thread child_thread(PrintSomething()), std::move(message));
+
+	/*
+	child thread cannot be a rvalue, ie, it cannot be copies, it has to be moved before doing that
+	*/
+	std::thread child_thread_2 = std::move(child_thread);
+
+	/*
+	After the move the child_thread will be empty or cannot be accessed
+	*/
+	//child_thread.join();
+	child_thread_2.join();
+	
+	/*
+	Id for child_thread_2,
+	This can also be initialized as this_thread inside the function where the child thread is alive or executes
+	*/
+	std::cout<<child_thread_2.get_id() << std::endl;
+
 	std::cout<<"From main:"<<message<<std::endl;
+	
+
 	// Can you try and catch to join the child_thread to the main thread
 	/*
 	try{
